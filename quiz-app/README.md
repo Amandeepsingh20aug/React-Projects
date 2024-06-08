1cb5a50db2b5b8b39a5449f150f60084d5654b66
amandeepsingh1929 <as0632568@gmail.com>
2024-06-08 13:11:37 +0530

    Added a feature and updated the Readme file again.

--- quiz-app/README.md
+++ quiz-app/README.md
@@ -3,4 +3,5 @@
 - Created Dummy json for Questiona and answer options
 - Created Question Component to diplay question and option to select
 - Created Result component to diplay number of correct question answered
+- Written the logic to show the correct answer to user for question which were wrong.
 - Also feature of retry the quiz.
--- quiz-app/src/components/Result.js
+++ quiz-app/src/components/Result.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Result = ({userAnswer,questions,resetQuiz}) => {
+  const[answer,setAnswer] = useState([]);
   const correctAnswer = () =>{
     let cnt  = 0;
    for(let i=0 ;i<userAnswer.length;i++){
@@ -9,6 +10,23 @@ const Result = ({userAnswer,questions,resetQuiz}) => {
    }
    return cnt;
   }
+
+
+  const filterAnswer = () =>{
+    const updatedAnswers =questions.flatMap(question=>
+      question.answerOptions.filter((option)=>(
+          option.isCorrect !== false
+      ))
+    )
+    setAnswer(updatedAnswers)
+  }
+
+
+  useEffect(() => {
+    filterAnswer();
+  }, [questions]);
+
+
   return (
     <div className='text-center my-4 border border-gray-400'>
       <h2 className='py-2 text-2xl font-medium'>Results</h2>
@@ -18,6 +36,7 @@ const Result = ({userAnswer,questions,resetQuiz}) => {
         {questions.map((question,index)=>(
           <li className={userAnswer[index] === true ? 'py-2 font-semibold text-xl text-green-300': 'py-2 font-semibold text-xl text-red-300'} key={index}>
             Q{index+1}. {question.question}
+            {userAnswer[index] === false ? <p className='text-black'>{answer[index] ? answer[index].text : ""}</p> : ''}
           </li>
         ))}
       </ul>
